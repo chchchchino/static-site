b9bf0d18a032ac1fb594b9dc830dc582759f7743
Gabe Chino <gabe.chino@gmail.com>
2018-12-30 19:24:17 -0700

    updates made to the static site

--- staticfiles.js
+++ staticfiles.js
@@ -5,8 +5,13 @@ const StaticFileHandler = require('serverless-aws-static-file-handler')
 
 module.exports.staticfile = (event, context, callback) => {
     // clientFilesPath is a local directory that serverless will automatically package and deploy to AWS Lambda along with your code
+    console.log(event)
     const clientFilesPath = path.join(__dirname, './gc/')
     return new StaticFileHandler(clientFilesPath).get(event, context)
-      .then(response => callback(null, response))
-      .catch(err => callback(err))
+  }
+
+  module.exports.binary = async (event, context) => {
+    //event.path = "png.png"
+    const clientFilesPath = path.join(__dirname, './gc/')
+    return new StaticFileHandler(clientFilesPath).get(event, context)
   }
